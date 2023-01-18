2ea32dad54daca274dbaf53eced1aa4c7d80c816
Adrianft25 <adrianft25@gmail.com>
2023-01-18 21:26:39 +0100

    Obtener fake data através del servicio

--- src/app/pages/cartas/cartas.component.ts
+++ src/app/pages/cartas/cartas.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Card } from 'src/app/models/carta.model';
+import { CartasService } from 'src/app/services/cartas.service';
 
 @Component({
   selector: 'app-cartas',
   templateUrl: './cartas.component.html',
   styleUrls: ['./cartas.component.scss']
 })
-export class CartasComponent {
+export class CartasComponent implements OnInit {
+
+  cards: Card[] = [];
+
+  constructor(private _cartasService: CartasService) {
+
+  }
+
+  ngOnInit(): void {
+
+    this._cartasService.getJSON().subscribe((data: Card[]) => {
+      this.cards = data;
+    });
+
+  }
 
 }
+
--- src/app/services/cartas.service.ts
+++ src/app/services/cartas.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Card } from '../models/carta.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartasService {
 
-  constructor() { }
+  private _jsonURL = 'assets/data/dataAPIyugioh.json';
+
+  constructor(private http: HttpClient) {
+    
+  }
+
+  public getJSON(): Observable<Card[]> {
+    return this.http.get<Card[]>(this._jsonURL);
+  }
+  
 }
