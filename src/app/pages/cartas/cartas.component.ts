a3a1d4826e4e572723baddaf590b1779839edff1
Adrianft25 <adrianft25@gmail.com>
2023-01-18 20:39:41 +0100

    Añadir componente Cartas

---
+++ src/app/pages/cartas/cartas.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cartas',
+  templateUrl: './cartas.component.html',
+  styleUrls: ['./cartas.component.scss']
+})
+export class CartasComponent {
+
+}
