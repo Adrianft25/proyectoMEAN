95ca24eabcfbe07a8b74a82c4b5b06679671736e
Adrianft25 <adrianft25@gmail.com>
2023-01-20 13:39:43 +0100

    Formulario y preparación para tratamiento de datos

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+
+  constructor(){}
+
+  ngOnInit(): void {
+  }
+
+  //Recuperar los datos del formulario de Login
+
+  login(form:NgForm) {
+
+    const email = form.value.email;
+    const pass = form.value.passwd;
+  }
 
 }
