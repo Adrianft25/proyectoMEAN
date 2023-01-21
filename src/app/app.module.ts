afcd8224737aba97ae3b67860ef0d3d0e164e5d8
Adrianft25 <adrianft25@gmail.com>
2023-01-21 12:18:04 +0100

    Imports y componentes añadidos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
@@ -8,6 +9,8 @@ import { CartasComponent } from './pages/cartas/cartas.component';
 import { HttpClientModule } from '@angular/common/http';
 import { CartaComponent } from './widgets/carta/carta.component';
 import { LoginComponent } from './pages/login/login.component';
+import { SignupComponent } from './pages/login/signup/signup.component';
+import { SigninComponent } from './pages/login/signin/signin.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
@@ -23,11 +26,14 @@ const routes: Routes = [
     HomeComponent,
     CartasComponent,
     CartaComponent,
-    LoginComponent
+    LoginComponent,
+    SignupComponent,
+    SigninComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
+    FormsModule,
     RouterModule.forRoot(routes)
   ],
   providers: [],
